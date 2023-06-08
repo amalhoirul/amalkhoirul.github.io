403cf953cf2b70b07acba41a3b1596300ca29a60
amalhoirul <135953418+amalhoirul@users.noreply.github.com>
2023-06-08 14:45:07 +0700

    Add files via upload

---
+++ blog.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Praktikum Rekayasa Web</title>   
+    </head>
+    <body>
+        <h1>Praktikum 7 mei 2023</h1>
+        <h1>NIM : G.231.20.0054</h1>
+        <h1>Nama : Muhammad Khoirul Amal</h1>
+    </body>
+</html>
